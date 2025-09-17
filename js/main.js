4d241a41ddc2aa7bb6d03ae27b7957fb04fa2071
Nicolas Pinzon <np07qsf@gmail.com>
2025-09-17 16:24:49 -0500

    Creamos las constantes para capturar elementos principales globales

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('form');
+const cont_global = document.getElementById('main-container');
+const completedItems = document.getElementById('cont-items-completed');
+const itemsToDo = document.getElementById('cont-to-do-list');
+const addBtn = document.getElementById('button_form');
+const
